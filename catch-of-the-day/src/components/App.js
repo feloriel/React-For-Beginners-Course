f2361e894a7c5babe0d7f043cd88d24b149be8cc
Irina Lepekhina <irina.lepekhina95@gmail.com>
2019-10-09 12:00:49 +0300

    understanding state

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -4,6 +4,21 @@ import Order from './Order';
 import Inventory from './Inventory';
 
 class App extends React.Component {
+  state = {
+    fishes: {},
+    order: {}
+  };
+  addFish = fish => {
+    // this.state.fishes.push(fish); // If it's an array in Vanilla JS
+    // this.state.fishes.fish1 = fish; // If it's an object in Vanilla JS
+
+    // 1. Take a copy of the existing state
+    const fishes = {...this.state.fishes}; // Object spread
+    // 2. Add our new fish to tha fishes variable
+    fishes[`fish${Date.now()}`] = fish;
+    // 3. Set the new fishes object to state
+    this.setState({ fishes }); // The same as fishes: fishes
+  };
   render() {
     return (
       <div className="catch-of-the-day">
@@ -11,7 +26,7 @@ class App extends React.Component {
           <Header tagline="Fresh Seafood Market" />
         </div>
         <Order />
-        <Inventory />
+        <Inventory addFish={this.addFish} />
       </div>
     )
   }
